2c070f9d86070706190ad15e82ab6d09b3c5f845
Karan Bhargava <karan@Karans-MacBook-Pro.local>
2017-12-19 18:24:08 -0800

    Initial setup of different components and ReactJS

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component. This compement should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//ES2016 syntax (similar to var but a constant)
+// const App = function() {
+//   //JSX -> subset of JS that allows us to write HTML like JS
+//   return <div>Hi!</div>;
+// }
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Rule -> 1 component per file
+
+//ES6 syntax
+const App = () => {
+  //JSX -> subset of JS that allows us to write HTML like JS
+  return <div>Hi!</div>;
+}
+
+//Valid JSX if empty tag -> <app />
+
+/**
+Get's converted to HTML from JSX
+return <o1>
+        <l1> 1 </l1>
+      </ol>;
+*/
+//Why use JSX? Creates HTML that is pushed to the DOM
+
+// Take this compement's generated HTML and put on page (in the DOM)
+
+//Doesn't work (react not defined)
+// React.render(App)
+
+//Also need to create the reactDOM. React JS only knows how to create elements
+// Currently passing class and not the instance of it
+// ReactDOM.render(App)
+
+// Container is not a DOM element
+// ReactDOM.render(<App />)
+
+//Need to provide target DOM (where to render it on the page)
+ReactDOM.render(<App />, document.querySelector('.container'));

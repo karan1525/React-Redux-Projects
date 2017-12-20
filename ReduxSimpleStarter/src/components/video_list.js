3b5498d59e4808789944298303785bfe8373d621
Karan Bhargava <karan1525@yahoo.co.in>
2017-12-19 23:26:30 -0800

    Setup thumbnails and list on main page

--- ReduxSimpleStarter/src/components/video_list.js
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import VideoListItem from './video_list_item';
 
 //Class based -> props are always available 
 // Function based -> only when passed in a function
 
+// Stay away from for loops -> use maps
+// var array = [1,2,3]
+// array.map(function(number) { return number * 2} )
+// passes all the values into the function
+
 const VideoList = (props) => {
+
+	const videoItems = props.videos.map((video) => {
+		return <VideoListItem key={video.etag} video={video} />
+	});
+
 	return (
 		<ul className="col-md-4 list-group">
-		{props.videos.length}
+			{videoItems}
 		</ ul>
 	);
 };

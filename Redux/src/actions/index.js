2cdd3da5d84c43bcebe7a58b12475eff9b915d4e
Karan Bhargava <karan1525@yahoo.co.in>
2017-12-22 00:28:50 -0800

    ActionCreator is now being sent to all reducers

--- Redux/src/actions/index.js
+++ Redux/src/actions/index.js
@@ -1,3 +1,9 @@
-export default function selectBook(book) {
-  console.log('A Book has been selected', book.title);
+export function selectBook(book) {
+  // Select book is an actionCreator, it needs to return an action,
+  // an object with a type property
+
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }

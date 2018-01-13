40baa64a165a24efcbadb4f0f62081acc1b017fc
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-12 18:54:53 -0800

    Initial setup

--- Indecision/src/App.js
+++ Indecision/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World!</h1>
+        <h1>Indecision App</h1>
+        Put your life in the hands of a computer
       </div>
     );
   }
